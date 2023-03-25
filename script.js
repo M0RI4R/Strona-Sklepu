f06fbfaab777fd429e42d92013a29e9181e4867d
M0RI4R <michalst3c@gmail.com>
2023-03-25 19:20:06 +0100

    Formularz logowania zamienia sie z formularzem rejestracji

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // ------------------------BUTTONS------------------------------
 const burger = document.querySelector(".burger");
-const submitPopup = document.querySelector(".btn-submit");
+let submitPopup = document.querySelector(".btn-submit");
 const btnColeForm = document.querySelector(".close-form");
 const btnBrowseCat = document.querySelector(".browse-button");
+const changeForm = document.querySelector(".change-form");
 
 //-------------------------ICONS--------------------------------
 const barsIcon = document.querySelector(".fa-bars");
 const xmark = document.querySelector(".x-mark");
 const userBtn = document.querySelector(".fa-user");
+let headerPopup = document.querySelector(".header-popup");
+const emailFormInput = document.querySelector(".mail-input");
 
 //---------------------------CONTEINERS-------------------------
 const navConteiner = document.querySelector("nav");
 const signInPopup = document.querySelector(".sign-in-form");
+const formEmailPopup = document.querySelector(".form-email-popup");
 const body = document.querySelector("body");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 
@@ -30,19 +34,35 @@ const loginPopup = () => {
   body.classList.add("blurr");
 };
 const submitForm = () => {
+  signInPopup.classList.remove("active-form");
   body.classList.remove("blurr");
 };
 const close = () => {
   signInPopup.classList.remove("active-form");
   body.classList.remove("blurr");
 };
-const categoriesList = (e) => {
+const categoriesList = () => {
   dropDownMenu.classList.toggle("dropdown-menu-active");
+};
+const changeFormSign = () => {
+  formEmailPopup.classList.toggle("form-email-popup");
+  emailFormInput.setAttribute("required", true);
+
+  if (formEmailPopup.classList != "form-email-popup") {
+    submitPopup.textContent = "Sign up";
+    headerPopup.textContent = "Sign up";
+    changeForm.textContent = "Are you a member? Sign in";
+  } else {
+    submitPopup.textContent = "Sign in";
+    headerPopup.textContent = "Sign in";
+    changeForm.textContent = "Are you not a member? Sign up";
+  }
 };
 
 //----------------------------ADD-EVENT-LISTENERS----------------
 burger.addEventListener("click", menu);
 userBtn.addEventListener("click", loginPopup);
-submitPopup.addEventListener("click", submitForm);
+submitPopup.addEventListener("submit", submitForm);
 btnColeForm.addEventListener("click", close);
 btnBrowseCat.addEventListener("click", categoriesList);
+changeForm.addEventListener("click", changeFormSign);
